67a120130b5154b428a1f3414fd8e068844aeb80
Miaoqi Chu axinite <mqichu@anl.gov>
2022-01-28 11:24:26 -0600

    render two htmls

--- html_utlits.py
+++ html_utlits.py
@@ -19,35 +19,45 @@ def convert_to_html(save_dir, data_dict):
     title = os.path.basename(save_dir)
 
     subs = jinja2.Environment(loader=jinja2.FileSystemLoader(
-        './')).get_template('template_single.html').render(title=title, mydata=data_dict)
+        './')).get_template('template/single.html').render(title=title, mydata=data_dict)
 
     # lets write the substitution to a file
     with open(outputfile, 'w') as f:
         f.write(subs)
 
 
-def combine_all_htmls(fname='index.html', target_folder='html'):
+def combine_all_htmls(target_folder='html'): 
+    targets = ['index.html', 'preview.html']
     files = os.listdir(target_folder)
     htmls = [x for x in files if x.endswith('.html')]
-    if 'index.html' in htmls:
-        htmls.remove('index.html')
     htmls.sort()
 
     html_info = []
     for x in htmls:
+        if x in targets:
+            continue
         short_label = os.path.splitext(x)[0]
         json_fname = os.path.join(target_folder, short_label, 'metadata.json') 
-        with open(json_fname, 'r') as f:
-            meta = json.load(f)
-        html_info.append([
-            short_label, x, meta['analysis_type'], meta['t_begin']
-        ])
-    subs = jinja2.Environment(
-        loader=jinja2.FileSystemLoader('./')
-    ).get_template('template_combined.html').render(mydata=html_info)
-    # lets write the substitution to a file
-    with open(os.path.join(target_folder, fname), 'w') as f:
-        f.write(subs)
+        try:
+            with open(json_fname, 'r') as f:
+                meta = json.load(f)
+                v1, v2 = meta['analysis_type'], meta['t_begin']
+        except Exception:
+            pass
+        else:
+            html_info.append([
+                short_label, x, v1, v2
+            ])
+
+    tfiles = ['template/combined.html', 'template/combined_preview.html']
+
+    for target, template in zip(targets, tfiles):
+        subs = jinja2.Environment(
+            loader=jinja2.FileSystemLoader('./')
+        ).get_template(template).render(mydata=html_info)
+        # lets write the substitution to a file
+        with open(os.path.join(target_folder, target), 'w') as f:
+            f.write(subs)
 
     copy_minipreview(target_folder)
 
