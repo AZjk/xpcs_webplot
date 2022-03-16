656098f64b0c1ca255994d5c5313ed1b4d6eb9b0
Miaoqi Chu axinite <mqichu@anl.gov>
2022-03-16 14:38:06 -0500

    allow both globus plot and local_plot

--- xpcs_webplot/cli.py
+++ xpcs_webplot/cli.py
@@ -4,12 +4,15 @@ single hdf plot tool for globus
 from .plot_images import hdf2web_safe as hdf2web
 import argparse
 import logging
+import sys
+from .webplot_cli import local_plot
+
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s.%(msecs)03d %(name)s %(levelname)s | %(message)s',
                     datefmt='%m-%d %H:%M:%S')
 
-def main():
+def globus_plot():
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('fname', metavar='FNAME', type=str,
                         help='the hdf filename')
@@ -32,5 +35,15 @@ def main():
     hdf2web(fname, image_only=True, **kargs)
 
 
+def main():
+    # local method for 8IDI, it will compile the image to html
+    # it reads the setting from .xpcs_webplot/default_setting.json
+    if len(sys.argv[0]) > 1 and sys.argv[1] == '__local__':
+        return local_plot()
+    # for globus plot. only plot images;
+    else:
+        return globus_plot()
+
+
 if __name__ == '__main__':
-    main()
+    sys.exit(main())
